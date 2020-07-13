8fc013691f6a45ac479262deb93148ba27d81ac5
Shahed <shaheduz.zaman@northsouth.edu>
2020-07-14 01:06:01 +0600

    Objects literals basic

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ console.log(year);
 
 
 //Data types: String, number, boolean, null, undefined, symbols(less used)
+console.warn("Data Types....................")
 const name = 'John';
 const age2 = 40;
 const driverRating = 4.7;
@@ -52,12 +53,59 @@ console.log(cars.split(', '));
 
 
 //Arrays
+console.warn("Arrays..............")
 const rolls = new Array(1, 2, 3, 4, 5, 6);
 console.log(rolls);
 const games = ['GTA5', 'ASC', 'FC', 'COD', 'NFS', 'CS', 'DOTA', 22, 563, 67857, true, false, null, undefined];
 games.push('PUBG');         //to add at the last
 games.unshift('RDR2');      //to add at the begining
 games.pop();                //delete the last item
-console.warn("Array Validity: "+Array.isArray(games));
+console.log("Array Validity: "+Array.isArray(games));
 console.log(games);
 console.log(games[1]);
+
+
+
+/***********************************************************/
+
+//Object Literals
+console.warn("Objects Literals.....................");
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    personAge: 25,
+    hobbies: ['Cycling', 'Movies', 'Music', 'Gaming'],
+    address:{
+        Street:'Road #12345',
+        Area: 'Bashundhara R/A',
+        City: "Dhaka"
+    }
+}
+console.log(person);
+console.log(person.hobbies[2]);
+console.log(person.address.Area);
+person.email = 'john@email.com';
+console.log(person);
+
+//Arrays of Objects
+
+const ToDos=[
+    {
+        id: 1,
+        Task:"Do the homework",
+        isDone: false
+    },
+    {
+        id: 2,
+        Task:"Finish PS lesson",
+        isDone: false
+    },
+    {
+        id: 3,
+        Task:"Watch Tv series",
+        isDone: false
+    }
+];
+
+console.log(ToDos);
+console.log(ToDos[2].Task, ToDos[2].isDone);
